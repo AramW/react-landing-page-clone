c56094be52390e9bd369c47eafff20c91d2fb739
Aram Weber <tidus.zakk@gmail.com>
2023-01-23 18:07:01 +0100

    Create first app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>my header</header>
+      <main>my main</main>
+      <footer>footer</footer>
+    </>
   );
 }
 
 export default App;
+
+//22:35 YV.Copying a Landing Page with CSS-in-JS
